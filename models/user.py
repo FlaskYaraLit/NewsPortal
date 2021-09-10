937e53f05f4be97065d709b440f0590ff8f1f1f6
Student <student@lan.itstep.kiev.ua>
2021-09-10 21:22:35 +0300

    добавлен контроллер регистрации и модель user

--- models/user.py
+++ models/user.py
@@ -5,7 +5,16 @@ class User(object):
 
     @staticmethod
     def register() -> None: # self отсутствует, т.к. статический метод
-        pass
+        connection = mysql.get_db()
+        cursor = connection.cursor()  # создаём курсор
+        sql_query = """
+            insert into users (login, password, email, regdate, status)
+            values (%s, #%s, %s, %s)
+        """
+        cursor.execute(sql_query, (login, password, email, regdate, status))
+        connection.commit()
+        cursor.close()
+        connection.close()
 
     @staticmethod
     def login() -> None:
